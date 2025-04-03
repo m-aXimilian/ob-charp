d998318d883e743b4047d5e971825455581af59f
m-aXimilian <keuffnermax@gmail.com>
2025-04-03 23:30:14 +0200

    add tests and test data

--- test-ob-csharp.el
+++ test-ob-csharp.el
@@ -32,7 +32,7 @@
 (ert-deftest test-ob-csharp/lackmus ()
   (should (= 1 1)))
 
-(org-test-for-executable "dotnet")
+(org-test-for-executable org-babel-csharp-compiler)
 
 (defun find-dotnet-version ()
   "Get a list of dotnet major versions from a list of dotnet sdks."
@@ -40,9 +40,80 @@
               (let ((fr (string-match "^[0-9.]+\\." n))
                     (to (string-match "\\." n)))
                 (string-to-number (substring n fr to))))
-          (split-string (shell-command-to-string "dotnet --list-sdks") "\n")))
+          (split-string (shell-command-to-string (format "%s --list-sdks" org-babel-csharp-compiler)) "\n")))
 
-(defvar dotnet-version (format "net%s.0" (apply #'max (find-dotnet-version))))
+(defvar system-dotnet-version (format "net%s.0" (apply #'max (find-dotnet-version)))
+  "The most recent dotnet version of the host system.")
+
+(defun try-set-dotnet-version (version)
+  ""
+  (when version
+    (setq org-babel-csharp-default-target-framework version)))
+
+(defmacro with-newest-dotnet (&rest body)
+  "Set the most recent dotnet version in context."
+  `(progn
+     (try-set-dotnet-version system-dotnet-version)
+     ,@body))
+
+(ert-deftest test-ob-csharp/int-from-var ()
+  "Test of an integer variable."
+  (with-newest-dotnet
+   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
+     (org-babel-next-src-block 1)
+     (should (= 42 (org-babel-execute-src-block))))))
+
+(ert-deftest test-ob-csharp/float-from-var ()
+  "Test of a float variable."
+  (with-newest-dotnet
+   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
+     (org-babel-next-src-block 2)
+     (should (= 3.14 (org-babel-execute-src-block))))))
+
+(ert-deftest test-ob-csharp/string-from-var ()
+  "Test of a string variable."
+  (with-newest-dotnet
+   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
+     (org-babel-next-src-block 3)
+     (should (string= "pi" (org-babel-execute-src-block))))))
+
+(ert-deftest test-ob-csharp/outputs-list ()
+  "Test list output."
+  (with-newest-dotnet
+   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
+     (org-babel-next-src-block 4)
+     (should (equal "Item 1\nItem 2\nItem 3\nItem 4\n" (org-babel-execute-src-block))))))
+
+(ert-deftest test-ob-csharp/commandline-input ()
+  "Test command line input."
+  (with-newest-dotnet
+   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
+     (org-babel-next-src-block 5)
+     (should (= 42 (org-babel-execute-src-block))))))
+
+(ert-deftest test-ob-csharp/custom-class-and-main ()
+  "Test custom class with custom main function."
+  (with-newest-dotnet
+   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
+     (org-babel-next-src-block 6)
+     (should (= 123 (org-babel-execute-src-block))))))
+
+(ert-deftest test-ob-csharp/tabular-format-output ()
+  "Test for tabular output format."
+  (with-newest-dotnet
+   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
+     (org-babel-next-src-block 7)
+     (should (equal '(("In" "questo" "mondo" "una" "cosa")
+                      ("si" "perde" "una" "si" "trova"))
+                    (org-babel-execute-src-block))))))
+
+(ert-deftest test-ob-csharp/nuget-reference ()
+  "Test with nuget reference."
+  (with-newest-dotnet
+   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
+     (org-babel-next-src-block 8)
+     (should (string= "{\n  \"TheInt\": 12,\n  \"TheString\": \"ok\"\n}\n"
+                    (org-babel-execute-src-block))))))
 
 (provide 'test-ob-csharp)
 ;;; test-ob-csharp.el ends here
