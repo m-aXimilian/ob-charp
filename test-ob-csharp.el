bc5702d65e6685f57ee653783c21d18fe714b033
m-aXimilian <keuffnermax@gmail.com>
2025-04-15 22:17:59 +0200

    [code review 4] add "with-" prefix & inline source blocks

--- test-ob-csharp.el
+++ test-ob-csharp.el
@@ -29,11 +29,6 @@
 (unless (featurep 'ob-csharp)
   (signal 'missing-test-dependency '("Support for C# code blocks")))
 
-(ert-deftest test-ob-csharp/default-compile-command-created ()
-  "The default compile command function creates expected command."
-  (should (string= (org-babel--csharp-default-compile-command "/tmp/placeholder/dummy.csproj" "/tmp/placeholder/bin")
-                   "dotnet build --output \"/tmp/placeholder/bin\" \"/tmp/placeholder/dummy.csproj\"")))
-
 (ert-deftest test-ob-csharp/customized-compile-command-used ()
   "User specified compile command is used."
   (let* ((custom-fun (lambda (p b) (format "custom-compiler %s %s" p b)))
@@ -50,25 +45,21 @@
     ;; reset customized variable
     (setq org-babel-csharp-generate-compile-command cmd-backup)))
 
-(ert-deftest test-ob-csharp/default-restore-command-created ()
-  "The default project restore command function creates expected command."
-  (should (string= (org-babel--csharp-default-restore-command "/tmp/placeholder/dummy.csproj")
-                   "dotnet restore \"/tmp/placeholder/dummy.csproj\"")))
-
 (ert-deftest test-ob-csharp/customized-restore-command-used ()
   "User specified compile command is used."
   (let* ((custom-fun (lambda (p) (format "custom-restore %s" p)))
          (project "/tmp/placeholder/dummy.csproj")
          (default-command (funcall org-babel-csharp-generate-restore-command project))
          (cmd-backup org-babel-csharp-generate-restore-command))
-    (setq org-babel-csharp-generate-restore-command custom-fun)
-    (should-not (string=
-                 default-command
-                 (funcall org-babel-csharp-generate-restore-command project)))
-    (should (string= (funcall custom-fun project)
-                     (funcall org-babel-csharp-generate-restore-command project)))
-    ;; reset customized variable
-    (setq org-babel-csharp-generate-restore-command cmd-backup)))
+    (unwind-protect
+        (progn
+          (setq org-babel-csharp-generate-restore-command custom-fun)
+          (should-not (string=
+                       default-command
+                       (funcall org-babel-csharp-generate-restore-command project)))
+          (should (string= (funcall custom-fun project)
+                           (funcall org-babel-csharp-generate-restore-command project))))
+      (setq org-babel-csharp-generate-restore-command cmd-backup))))
 
 (ert-deftest test-ob-csharp/generate-project-file ()
   "Test intended parameterization of the project file generator."
@@ -97,7 +88,7 @@
 ;; requires dotnet compiler
 (org-test-for-executable org-babel-csharp-compiler)
 
-(defun find-dotnet-version ()
+(defun with-find-dotnet-version ()
   "Get a list of dotnet major versions from a list of dotnet sdks."
   (mapcar #'(lambda (n)
               (let ((fr (string-match "^[0-9.]+\\." n))
@@ -105,10 +96,10 @@
                 (string-to-number (substring n fr to))))
           (split-string (shell-command-to-string (format "%s --list-sdks" org-babel-csharp-compiler)) "\n")))
 
-(defvar system-dotnet-version (format "net%s.0" (apply #'max (find-dotnet-version)))
+(defvar system-dotnet-version (format "net%s.0" (apply #'max (with-find-dotnet-version)))
   "The most recent dotnet version of the host system.")
 
-(defun try-set-dotnet-version (version)
+(defun with-try-set-dotnet-version (version)
   "Set the default framework version."
   (when version
     (setq org-babel-csharp-default-target-framework version)))
@@ -116,56 +107,88 @@
 (defmacro with-newest-dotnet (&rest body)
   "Set the most recent dotnet version in context."
   `(progn
-     (try-set-dotnet-version system-dotnet-version)
+     (with-try-set-dotnet-version system-dotnet-version)
      ,@body))
 
 (ert-deftest test-ob-csharp/int-from-var ()
   "Test of an integer variable."
   (with-newest-dotnet
-   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
-     (org-babel-next-src-block 1)
+   (org-test-with-temp-text "#+begin_src csharp :var i=42 :results silent
+  Console.WriteLine(i);
+#+end_src"
      (should (= 42 (org-babel-execute-src-block))))))
 
 (ert-deftest test-ob-csharp/float-from-var ()
   "Test of a float variable."
   (with-newest-dotnet
-   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
-     (org-babel-next-src-block 2)
+   (org-test-with-temp-text "#+begin_src csharp :var f=3.14 :results silent
+  Console.WriteLine(f);
+#+end_src"
      (should (= 3.14 (org-babel-execute-src-block))))))
 
 (ert-deftest test-ob-csharp/string-from-var ()
   "Test of a string variable."
   (with-newest-dotnet
-   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
-     (org-babel-next-src-block 3)
+   (org-test-with-temp-text "#+begin_src csharp :var s=\"pi\" :results silent
+  Console.WriteLine(s);
+#+end_src"
      (should (string= "pi" (org-babel-execute-src-block))))))
 
 (ert-deftest test-ob-csharp/outputs-list ()
   "Test list output."
   (with-newest-dotnet
-   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
-     (org-babel-next-src-block 4)
+   (org-test-with-temp-text "#+begin_src csharp :results raw list silent
+  Console.WriteLine(\"Item 1\");
+  Console.WriteLine(\"Item 2\");
+  Console.WriteLine(\"Item 3\");
+  Console.WriteLine(\"Item 4\");
+#+end_src"
      (should (equal "Item 1\nItem 2\nItem 3\nItem 4\n" (org-babel-execute-src-block))))))
 
 (ert-deftest test-ob-csharp/commandline-input ()
   "Test command line input."
   (with-newest-dotnet
-   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
-     (org-babel-next-src-block 5)
+   (org-test-with-temp-text "#+begin_src csharp :cmdline 3 :usings '(\"System\" \"System.Text\") :results silent
+  int argInt = 0;
+  Int32.TryParse(args[0], out argInt);
+
+  Console.WriteLine(argInt * 14);
+#+end_src"
      (should (= 42 (org-babel-execute-src-block))))))
 
 (ert-deftest test-ob-csharp/custom-class-and-main ()
   "Test custom class with custom main function."
   (with-newest-dotnet
-   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
-     (org-babel-next-src-block 6)
+   (org-test-with-temp-text "#+begin_src csharp :class no :main no :results silent
+  internal class ClassA
+  {
+      public ClassA(int i)
+      {
+          this.AnInt = i;
+      }
+
+      public int AnInt { get; set; }
+  }
+
+  public class Program
+  {
+      public static void Main(string[] args)
+      {
+          ClassA daInstance = new(123);
+
+          Console.WriteLine(daInstance.AnInt);
+      }
+  }
+#+end_src"
      (should (= 123 (org-babel-execute-src-block))))))
 
 (ert-deftest test-ob-csharp/tabular-format-output ()
   "Test for tabular output format."
   (with-newest-dotnet
-   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
-     (org-babel-next-src-block 7)
+   (org-test-with-temp-text "#+begin_src csharp :results table silent
+  Console.WriteLine($\"In, questo, mondo, una, cosa\");
+  Console.WriteLine($\"si, perde,  una,   si, trova\");
+#+end_src"
      (should (equal '(("In" "questo" "mondo" "una" "cosa")
                       ("si" "perde" "una" "si" "trova"))
                     (org-babel-execute-src-block))))))
@@ -173,10 +196,23 @@
 (ert-deftest test-ob-csharp/nuget-reference ()
   "Test with nuget reference."
   (with-newest-dotnet
-   (org-test-at-id "FC200076-9BB6-4070-B078-BB053262A8CD"
-     (org-babel-next-src-block 8)
+   (org-test-with-temp-text "#+begin_src csharp :references '((\"Newtonsoft.Json\" . \"13.0.3\")) :usings '(\"System\" \"Newtonsoft.Json\") :main no :project \"json-test\" :results verbatim silent
+  public class DTO
+  {
+      public int TheInt { get; set; }
+      public string TheString { get; set; }
+  }
+
+  static void Main(string[] args)
+  {
+      DTO myDto = new() { TheInt = 12, TheString = \"ok\" };
+
+      string json = JsonConvert.SerializeObject(myDto, Formatting.Indented);
+      Console.WriteLine($\"{json}\");
+  }
+#+end_src"
      (should (string= "{\n  \"TheInt\": 12,\n  \"TheString\": \"ok\"\n}\n"
-                    (org-babel-execute-src-block))))))
+                      (org-babel-execute-src-block))))))
 
 (provide 'test-ob-csharp)
 ;;; test-ob-csharp.el ends here
