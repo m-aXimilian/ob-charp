8bfe402122e48b0145deef86772310d47aa17efa
m-aXimilian <keuffnermax@gmail.com>
2025-04-21 20:31:38 +0200

    missing docstring

--- test-ob-csharp.el
+++ test-ob-csharp.el
@@ -278,6 +278,7 @@
 
 ;; requires at least 2 dotnet frameworks installed
 (ert-deftest test-ob-csharp/framework-header-is-configurable ()
+  "Check for additional framework header arguments."
   (ert--skip-when (< (length (test-ob-csharp--find-dotnet-version)) 2))
   (let* ((src-result (lambda (v) (org-test-with-temp-text
                                      (format "#+begin_src csharp :framework \"net%s.0\"
